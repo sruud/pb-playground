9ab77d33bce0440b2c89e59b38b0d7f1c48b1664
sruud <13337210+sruud@users.noreply.github.com>
2025-07-14 23:54:58 -0600

    demo: add example that sends audio to secondary vst(fx)

--- plugin_effect.py
+++ plugin_effect.py
@@ -12,10 +12,12 @@ system = platform.system()
 plugin_target = "Serum2"
 if system == "Darwin":
     vst_path = "/Library/Audio/Plug-Ins/VST3/{}.vst3".format(plugin_target)
+    vst_effect_path = "/Library/Audio/Plug-Ins/VST3/ValhallaSupermassive.vst3"
 elif system == "Windows":
     vst_path = r"C:\Program Files\Common Files\VST3\{0}.vst3\Contents\x86_64-win\{0}.vst3".format(
         plugin_target
     )
+    vst_effect_path = r"C:\Program Files\Common Files\VST3\ValhallaSupermassive.vst3"
 
 instrument = load_plugin(
     plugin_name="Serum 2",
@@ -27,13 +29,37 @@ instrument = load_plugin(
 #     path_to_plugin_file="C:\\Program Files\\Common Files\\VST3\\Serum2.vst3\\Contents\\x86_64-win\\Serum2.vst3",
 # )
 
+# TODO: find best way to load preset - testing to see if this works first
+# parameter_values= try mix 100 with high delay or something
+# Note the default does produce an audible effect
+effect = load_plugin(
+    plugin_name="ValhallaSupermassive",
+    path_to_plugin_file=vst_effect_path,
+)
+
 # pprint(effect.parameters.keys())
 # dict_keys([
-#   'sc_hpf_hz', 'input_lvl_db', 'sensitivity_db',
-#   'ratio', 'attack_ms', 'release_ms', 'makeup_db',
-#   'mix', 'output_lvl_db', 'sc_active',
-#   'full_bandwidth', 'bypass', 'program',
-# ])
+# 'mix',
+# 'delaysync',
+# 'delaynote',
+# 'delay_ms',
+# 'delaywarp',
+# 'clear',
+# 'feedback',
+# 'density',
+# 'width',
+# 'lowcut',
+# 'highcut',
+# 'modrate',
+# 'moddepth',
+# 'mode',
+# 'reserved1',
+# 'reserved2',
+# 'reserved3',
+# 'reserved4',
+# 'bypass']
+# )
+
 
 # Set the "ratio" parameter to 15
 # effect.ratio = 15
@@ -47,17 +73,18 @@ audio = instrument(
 )
 
 # Apply effects to this audio:
-# effected = effect(audio, sample_rate)
+effected = effect(audio, sample_rate)
 
 # ...or put the effect into a chain with other plugins:
 # board = Pedalboard([effect, Reverb()])
 # ...and run that pedalboard with the same VST instance!
 # effected = board(audio, sample_rate)
 
-
-with AudioFile("midi_to_audio_test.wav", "w", sample_rate, audio.shape[0]) as f:
-    f.write(audio)
+# with AudioFile("midi_to_audio_test.wav", "w", sample_rate, audio.shape[0]) as f:
+#     f.write(audio)
 
 # TODO: try this workflow next
-# with AudioFile('audio_fx_plugin_test.wav', 'w', sample_rate, effected.shape[0]) as f:
-#     f.write(effected)
+with AudioFile(
+    "audio_effects_plugin_test.wav", "w", sample_rate, effected.shape[0]
+) as f:
+    f.write(effected)
